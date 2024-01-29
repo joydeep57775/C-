86050e9f7caab68a954b57dbe024b9f680f26579
joydeep57775 <158074465+joydeep57775@users.noreply.github.com>
2024-01-29 21:41:58 +0530

    User

---
+++ input.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+string name;
+cout<<"Enter you name";
+cin>>name;
+cout<<"Your name is: "<<name<<endl;
+
+int age;
+cout<<"what is you age";
+cin>>age;
+cout<<"Your age is: "<<age<<endl;
+
+char gender;
+cout<<"What is your gender";
+cin>>gender;
+cout<<"Your gender is: "<<gender<<endl;
+
+float weight;
+cout<<"Enter you weight";
+cin>>weight;
+cout<<"Your weight is: "<<weight<<endl;
+
+return 0;
+}
